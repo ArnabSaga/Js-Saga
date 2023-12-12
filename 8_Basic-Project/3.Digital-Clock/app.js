70bd5ea285720de78ae7f5b801bbf4177f5dd92b
Aranb dey <arnabdey15091@gmail.com>
2023-12-12 22:36:04 +0600

    modified:   8_Basic-Project/3.Digital-Clock/app.js modified:   8_Basic-Project/3.Digital-Clock/style.css

---
+++ 8_Basic-Project/3.Digital-Clock/app.js
@@ -0,0 +1,8 @@
+const clock = document.querySelector('#clock');
+
+
+setInterval(() =>{
+    let date = new Date()
+    // console.log(date.toLocaleTimeString()); 
+    clock.innerHTML = date.toLocaleTimeString()   
+},1000)
